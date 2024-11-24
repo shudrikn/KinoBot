f4d9c6e63394c885a6fd6060704df29f7cd1411b
Nikolay Shudrik <shudrik@rutoken.ru>
2024-11-24 16:16:50 +0300

    Little fix

--- dialog.py
+++ dialog.py
@@ -29,8 +29,14 @@ class Dialog:
         print(user_id)
         self.user_id = user_id
         self.response_handler = None
+        self.previosly_message = None
         self.add_user()
         self.list_lists()
+        
+    def delete_prev_message(self):
+        if self.previosly_message:
+            bot.delete_message(self.user_id, self.previosly_message)
+            self.previosly_message = None
         
     def add_user(self):
         print("add_user")
@@ -47,78 +53,79 @@ class Dialog:
         # Добавляем кнопки со списками
         markup = types.InlineKeyboardMarkup()
         for movie_list in lists:
-            button = types.InlineKeyboardButton(movie_list[2], callback_data=json.dumps(('o', movie_list[0])))
+            button = types.InlineKeyboardButton(movie_list[1], callback_data=json.dumps(('o', movie_list[0])))
             markup.add(button)
 
-        markup.row(types.InlineKeyboardButton("Создать", callback_data=json.dumps('c')), types.InlineKeyboardButton("Импортировать", callback_data=json.dumps('i')))
+        markup.row(types.InlineKeyboardButton("Создать", callback_data=json.dumps('c')), types.InlineKeyboardButton("Добавить", callback_data=json.dumps('i')))
 
         self.response_handler = self.list_lists_handler
         if lists:
             message_text = "Выбери список"
         else:
             message_text = "Доступных списков нет"
-        bot.send_message(self.user_id, message_text, reply_markup=markup)
+        self.previosly_message = bot.send_message(self.user_id, message_text, reply_markup=markup).message_id
 
     def list_lists_handler(self, data):
         print("list_lists_handler")
+        result = False
+        if type(data) is str:
+            # создание нового списка
+            if data == 'c':
+                result = True
+                self.new_list()
+            # импорт чужого списка
+            elif data == 'i':
+                result = True
+                self.import_list()
+        # открытие списка
+        elif type(data) is tuple and len(data) == 2 and data[0] == 'o':
+            result = True
+            self.open_list_menu(data[1])
 
-        try:
-            parced_data = json.loads(data)
-            if type(parced_data) is str:
-                # создание нового списка
-                if parced_data == 'c':
-                    self.new_list()
-                # импорт чужого списка
-                elif parced_data == 'i':
-                    self.import_list()
-            # открытие списка
-            elif type(parced_data) is tuple and len(parced_data) == 2 and parced_data[0] == 'o':
-                self.list_menu(parced_data[1])
-        except:
-            print("list_lists_handler exception")
+        if not result:
+            self.list_lists()
    
     def new_list(self):
         print("new_list")
         markup = types.InlineKeyboardMarkup()
         markup.row(types.InlineKeyboardButton("Назад", callback_data=json.dumps('e')))
         self.response_handler = self.new_list_handler
-        bot.send_message(self.user_id, "Введи имя списка", reply_markup=markup)
+        self.previosly_message = bot.send_message(self.user_id, "Введи имя списка", reply_markup=markup).message_id
         
     def new_list_handler(self, data):
         print("new_list_handler")
-        success = False
-        try:
-            parced_data = json.loads(data)
-            if type(parced_data) is str:
-                # назад
-                if parced_data == 'e':
-                    success = True
-                # введено имя нового списка
-                elif len(parced_data) > 0:
-                    with connection:
-                        cursor.execute('INSERT INTO List VALUES (NULL,?,?) RETURNING list_id', (parced_data,self.user_id))
-                        list_id = cursor.fetchone()
-                        cursor.execute('INSERT INTO UsersLists VALUES (?,?,?)', (self.user_id,list_id[0],Rights.owner))
-        except:
-            print("new_list_handler exception")
-        finally:
-            self.list_lists()
-        
+        result = False
+        if type(data) is str:
+            # назад
+            if data == 'e':
+                result = True
+            # введено имя нового списка
+            elif len(data) > 0:
+                with connection:
+                    cursor.execute('INSERT INTO List VALUES (NULL,?,?) RETURNING list_id', (data,self.user_id))
+                    list_id = cursor.fetchone()
+                    cursor.execute('INSERT INTO UsersLists VALUES (?,?,?)', (self.user_id,list_id[0],Rights.owner))
+                    result = True
+
+        if not result:
+            self.new_list()
+        else:
+            self.list_lists()        
 
     def import_list(self):
         print("import_list")
                 
     # работа со списками
-    def list_menu(self, list_id):
+    def open_list_menu(self, list_id):
         print("list_menu")
 
-    def list_movies(self):
+    def list_movies(self, list_id):
         print("list_movies")
 
-    def list_all_movies(self):
+    def list_all_movies(self, list_id):
         print("list_movies")
     
-    def add_movie_to_list(self):
+    def add_movie_to_list(self, list_id):
         print("add_movie_to_list")
         
     def share_list(self):
@@ -143,19 +150,23 @@ def start(message):
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     user_id = message.from_user.id
-    if not(user_id in handlers):
-        handlers[user_id] = Dialog(user_id)
-    else:    
-        handlers[user_id].response_handler(json.dumps(message.text))
+    handle_all(user_id, message.text)
+    # удалить сообщение пользователя
+    bot.delete_message(user_id, message.id)     
 
 # Обработка нажатий кнопок
 @bot.callback_query_handler(func=lambda call: True)
-def handle(call):
+def handle_button(call):
     user_id = call.from_user.id
+    handle_all(user_id, json.loads(call.data))
+
+# Обработчик всех действий пользователя       
+def handle_all(user_id, data):
     if not(user_id in handlers):
         handlers[user_id] = Dialog(user_id)
     else:
-        handlers[user_id].response_handler(call.data)
+        handlers[user_id].delete_prev_message()
+        handlers[user_id].response_handler(data)
 
 # Запускаем бота
 apihelper.RETRY_ON_ERROR = True        
